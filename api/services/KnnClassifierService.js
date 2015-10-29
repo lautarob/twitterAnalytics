998b91ebeb40deafde1323db161c3546c39f5f81
Lautaro Bertuzzi <lautaro@agilityfeat.com>
2015-10-29 02:28:14 -0300

    algorithms utilization

--- api/services/KnnClassifierService.js
+++ api/services/KnnClassifierService.js
@@ -5,7 +5,21 @@ var clasiffier = require('nearest-neighbor');
 var fields_importance = [];
 
 // var items = [
-//   { name: "Bill", age: 10, pc: "Mac", ip: "68.23.13.8" },
+//   {
+            //   "topic1":"",
+            //   "topic2":"",
+            //   "topic3":"",
+            //   "entitie1":"",
+            //   "entitie2":"",
+            //   "entitie3":"",
+            //   "hashtag1":"",
+            //   "hashtag2":"",
+            //   "hashtag3":"",
+            //   "keyword1":"",
+            //   "keyword2":"",
+            //   "keyword3":"",
+            //   "principalTopic":""
+            // },
 //   { name: "Alice", age: 22, pc: "Windows", ip: "193.186.11.3" },
 //   { name: "Bob", age: 12, pc: "Windows", ip: "56.89.22.1" }
 // ];
@@ -16,6 +30,21 @@ var fields_importance = [];
 //   { name: "pc", measure: clasiffier.comparisonMethods.word }, 
 //   { name: "ip", measure: clasiffier.comparisonMethods.ip }
 // ];
+
+var fields_importance = [
+  { name: "topic1", measure: clasiffier.comparisonMethods.word },
+  { name: "topic2", measure: clasiffier.comparisonMethods.word },
+  { name: "topic3", measure: clasiffier.comparisonMethods.word }, 
+  { name: "entitie1", measure: clasiffier.comparisonMethods.word },
+  { name: "entitie2", measure: clasiffier.comparisonMethods.word },
+  { name: "entitie3", measure: clasiffier.comparisonMethods.word }, 
+  { name: "hashtag1", measure: clasiffier.comparisonMethods.word },
+  { name: "hashtag2", measure: clasiffier.comparisonMethods.word },
+  { name: "hashtag3", measure: clasiffier.comparisonMethods.word },
+  { name: "keyword1", measure: clasiffier.comparisonMethods.word },
+  { name: "keyword2", measure: clasiffier.comparisonMethods.word },
+  { name: "keyword3", measure: clasiffier.comparisonMethods.word }
+  ];
 
 // var query = { name: "Bob", age: 12, pc: "Windows", ip: "68.23.13.10" };
 
