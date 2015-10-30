7e4e9f56a0295ace163410f958e8f3a0b43e44d0
Lautaro Bertuzzi <lautaro@agilityfeat.com>
2015-10-30 01:12:35 -0300

    analyser fixes

--- api/services/TwitterAnalyserService.js
+++ api/services/TwitterAnalyserService.js
@@ -26,70 +26,42 @@ module.exports = {
                 for (var i = 0; i < tweetProcessed.entities.length; i++) {
                     if(tweetProcessed.entities[i].text.indexOf("http") < 0)
                     {
-                        if(entities.length < 4)
-                        {
-                            entities.push(tweetProcessed.entities[i].text);
-                            switch(tweetProcessed.entities[i].type) {
-                                case "Country":
-                                case "City":
-                                case "StateOrCounty":
-                                    geography.push(tweetProcessed.entities[i].text);
-                                    break;
-                                case "Hashtag":
-                                    hashTags.push(tweetProcessed.entities[i].text);
-                                    break;
-                                case "TwitterHandle":
-                                    users.push(tweetProcessed.entities[i].text);
-                                    break;
-                                case "Person":
-                                    persons.push(tweetProcessed.entities[i].text);
-                                    break;
-                            }   
-                        }
-                        else
-                        {
-                            break;
-                        }
+                        entities.push(tweetProcessed.entities[i].text);
+                        switch(tweetProcessed.entities[i].type) {
+                            case "Country":
+                            case "City":
+                            case "StateOrCounty":
+                                geography.push(tweetProcessed.entities[i].text);
+                                break;
+                            case "Hashtag":
+                                hashTags.push(tweetProcessed.entities[i].text);
+                                break;
+                            case "TwitterHandle":
+                                users.push(tweetProcessed.entities[i].text);
+                                break;
+                            case "Person":
+                                persons.push(tweetProcessed.entities[i].text);
+                                break;
+                        }   
                     }
                 }
 
                 for (var i = 0; i < tweetProcessed.keywords.length; i++) {
                     if(tweetProcessed.keywords[i].text.indexOf("http") < 0)
                     {
-                        if(entities.length < 4)
-                        {
-                            keys.push(tweetProcessed.keywords[i].text);
-                        }
-                        else
-                        {
-                            break;
-                        }
+                        keys.push(tweetProcessed.keywords[i].text);
                     }
                 }
 
                 if(tweetProcessed.taxonomies.length > 0){
                     for (var i = 0; i < tweetProcessed.taxonomies.length; i++)
                     {
-                        if(topics.length < 4)
-                        {
-                            if(parseFloat(tweetProcessed.taxonomies[i].score) > 0.4){
-                                var topics_by_taxonomy = tweetProcessed.taxonomies[i].label.substring(1).split('/');
-                                for(var j = topics_by_taxonomy.length-1; j > -1; j--)
-                                {
-                                    if(topics.length < 4)
-                                    {
-                                        topics = topics.concat(topics_by_taxonomy[j]);  
-                                    }
-                                    else
-                                    {
-                                        break;
-                                    }
-                                }
+                        if(parseFloat(tweetProcessed.taxonomies[i].score) > 0.4){
+                            var topics_by_taxonomy = tweetProcessed.taxonomies[i].label.substring(1).split('/');
+                            for(var j = topics_by_taxonomy.length-1; j > -1; j--)
+                            {
+                                topics = topics.concat(topics_by_taxonomy[j]);  
                             }
-                        }
-                        else
-                        {
-                            break;
                         }
                     }
                 }
